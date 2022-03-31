acfe60d0d8abcdbc29bea6faca658be59ca1f911
OmarPadronPerez <omar.padron.perez@gmail.com>
2022-03-31 16:40:06 -0600

    se agrego

--- js/juego.js
+++ js/juego.js
@@ -5,7 +5,6 @@ function Nave(x){//nave principal
     this.posicionY=altoPan-25;
     this.ancho=20;
     this.alto=20;
-    //cargar imagen
     this.imagen=new Image();
     this.imagen=cargarImagen("imagenes/principal.png");
 }
@@ -20,16 +19,14 @@ function Bala(tipo,inicialX,inicialY,url){
     this.posicionY=inicialY;
 }
 
-/**function Enemigo1(x,y){
+function Ctie(x,y){//nave Enemiga
     this.posicionX=x;
     this.posicionY=y;
-    var ancho=5;
-    var alto=10
-    //cargar imagen
-    var imagen=new Image();
-    imagen.src=""; //buscar imagenes 
-    
-}*/
+    this.ancho=20; //  5/4  *4
+    this.alto=16;
+    this.imagen=new Image();
+    this.imagen=cargarImagen("imagenes/caza TIE.png");
+}
 
 /**iniciar pantalla */
 let pantalla = document.getElementById('pantalla').getContext('2d');
@@ -48,11 +45,11 @@ function cargarImagen(url){
 document.addEventListener('keydown', function(e){
     switch(e.key){
         case "ArrowRight":
-            naveP.posicionX=naveP.posicionX+2;
+            naveP.posicionX+=2;
         break;
 
         case "ArrowLeft":
-            naveP.posicionX=naveP.posicionX-2;
+            naveP.posicionX-=2;
         break;
 
         case " ":
@@ -69,61 +66,61 @@ document.addEventListener('keydown', function(e){
     //dibujarPantalla();
 });  
 
+
 /**funcion para mover balas */
 function moverBalas(){
-    /*arrayBalas.forEach((actual,index) => {
-        if(actual.tipo==0){// bala de jugador                  0 principal  1 enemiga
-            actual.posicionY=actual.posicionY+5;
-        }else{ //bala enemiga
-            actual.posicionY=actual.posicionY-5;
-        }
-
-        if(actual.posicionY<(-actual.alto-3)/*||                  //salio por arriba del canvas
-            actual.posicionY>altoPan+actual.alto+3){            //salio por abajo del canvas
-                console.log("bala fuera: "+arrayBalas.length);
-                arrayBalas=borrarElementoArray(arrayBalas,index);
-
-        }
-        
-    });*/
-    
-   
     let i=0
     for(i=0;i<arrayBalas.length;i++){
-        console.log("bala mover: "+arrayBalas);
+        //console.log("bala mover: "+arrayBalas);
         if(arrayBalas[i].tipo==0){// bala de jugador                  0 principal  1 enemigo
-            arrayBalas[i].posicionY=arrayBalas[i].posicionY+5;
+            arrayBalas[i].posicionY+=5;
         }else{ //bala enemiga
-            arrayBalas[i].posicionY=arrayBalas[i].posicionY-5;
+            arrayBalas[i].posicionY-=5;
         }
 
-        if(arrayBalas[i].posicionY<(-arrayBalas[i].alto-3)||              //salio por arriba del canvas
-        arrayBalas[i].posicionY>altoPan+arrayBalas[i].alto+3){            //salio por abajo del canvas
+        if(arrayBalas[i].posicionY<(-arrayBalas[i].alto-3)||    //salio por arriba del canvas
+        arrayBalas[i].posicionY>altoPan+arrayBalas[i].alto+3){  //salio por abajo del canvas
 
-            console.log("bala fuera: "+arrayBalas.length);
+            //console.log("bala fuera: "+arrayBalas.length);
             arrayBalas=borrarElementoArray(arrayBalas,i);
             i--;
         }
+        /**coliciones*/
+
     }
 }
 
-/**borra elementos del arreglo*/
-function borrarElementoArray(array,borrar){
-    let nuevo =array.splice(borrar,1);
-    console.log("borado: "+nuevo);
-    return nuevo;
+/**crea e inicia enemigos  */
+function iniciarEnemigos(){
+    let ancho=10;
+    let alto1=5
+    let alto2=30
+    let distancia= 26;
+    let alto=0;
+    //agregar un for y ver cuantos caben y separacion 
+    while(ancho+distancia+10<anchoPan){
+
+        for(let i=0;i<2;i++){
+            if(alto==alto1){
+                alto=alto2;
+            }else{
+                alto=alto1;
+            }
+            let naveP=new Ctie(ancho, alto);//(x,y) crear naves enemigas
+            arrayEnemigos.push(naveP);
+            ancho+=distancia;
+        }
+        
+    }    
 }
 
 
-/**funcion principal*/
-    const arrayBalas=[];
-    const arrayEnemigos=[];
-    let contTiem=0;
-
-    /**inicar naves*/
-
-    let naveP=new Nave(anchoPan/2-10);//crear nave jugador
-
+/**borra elementos del arreglo*/
+function borrarElementoArray(array,borrar){
+    const nuevo =Array.from(array.splice(borrar,1));
+    //console.log("borado: "+nuevo);
+    return nuevo;
+}
 
 /**funcion para dibujar la pantalla*/
 function dibujarPantalla(){
@@ -133,13 +130,26 @@ function dibujarPantalla(){
         pantalla.drawImage(actual.imagen, actual.posicionX, actual.posicionY, actual.ancho, actual.alto);
     });
 
+    arrayEnemigos.forEach((enemigo)=>{
+        pantalla.drawImage(enemigo.imagen, enemigo.posicionX, enemigo.posicionY, enemigo.ancho, enemigo.alto);
+    });
+
     pantalla.drawImage(naveP.imagen, naveP.posicionX, naveP.posicionY, naveP.ancho, naveP.alto);//dibujar jugador
 }
 
+/**funcion principal*/
+    const arrayBalas=[];
+    const arrayEnemigos=[];
+    let contTiem=0;
+
+    /**inicar naves*/
+    let naveP=new Nave(anchoPan/2-10);//crear nave jugador
+    iniciarEnemigos();
+
 /**mover naves Enemigas, mover balas, dibujar todo */
-setInterval(() => { 
-    //console.log("balas "+arrayBalas);
-    moverBalas();
-    dibujarPantalla();
-    contTiem++;
-}, 100);
+    setInterval(() => { 
+        //console.log("balas "+arrayBalas);
+        moverBalas();
+        dibujarPantalla();
+        contTiem++;
+    }, 100);
